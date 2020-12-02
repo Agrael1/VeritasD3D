0beb5ef30b1952479ae9a44ec4536f72b4e80bbc
Agrael <Agrael@live.ru>
2020-12-02 19:24:21 +0100

    fixed info manager

--- WinD3D/Framework/DXGIInfoManager.h
+++ WinD3D/Framework/DXGIInfoManager.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "Framework\WinSetup.h"
 #include <wrl.h>
-#include "IDXGIInfoManager.h"
 #include <DXGIDebug.h>
 
-class DXGIInfoManager : public IDXGIInfoManager
+class DXGIInfoManager 
 {
 public:
 	DXGIInfoManager();
@@ -12,8 +11,8 @@ public:
 	DXGIInfoManager(const DXGIInfoManager&) = delete;
 	DXGIInfoManager& operator=(const DXGIInfoManager&) = delete;
 public:
-	void Set()noexcept override;
-	std::vector<std::string> GetMessages()const override;
+	void Set()noexcept;
+	std::vector<std::string> GetMessages()const;
 private:
 	unsigned long long next = 0Ui64;
 	Microsoft::WRL::ComPtr<IDXGIInfoQueue> pDxgiInfoQueue;
