b0b0d4710710159339464d804ca7c3158643e001
Ilya Doroshenko <agrael@live.ru>
2023-03-24 07:44:01 +0100

    removed junk

--- WinD3D/include/Engine/Pass/Loading/Image.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <DirectXTex.h>
-#include <optional>
-#include <string_view>
-
-namespace ver
-{
-	std::optional<DirectX::ScratchImage> LoadWICTexture(std::wstring_view path, bool mips = true);
-}
